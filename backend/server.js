bc111c44e63d79b05e095c816cb55dfe6c90d371
VaibhavMore64 <vaibhav.more@64-squares.com>
2025-05-25 01:43:38 +0530

    Creating Income API's

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors")
 const path = require("path")
 const connectDB=require("./config/db");
 const authRoutes=require("./routes/authRoutes")
+const incomeRoutes=require("./routes/incomeRoutes")
 const app=express();
 
 //Middleware to handle CORS
@@ -21,6 +22,7 @@ app.use(express.json());
 connectDB();
 
 app.use("/api/v1/auth",authRoutes);
+app.use("/api/v1/income",incomeRoutes);
 
 const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>console.log(`Server running on port ${PORT}`));
