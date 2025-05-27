0f1bb0880116f4d6bed8a424302aff8fe65b829f
VaibhavMore64 <vaibhav.more@64-squares.com>
2025-05-27 17:34:35 +0530

    Recent transactions component added

--- frontend/src/utils/helper.js
+++ frontend/src/utils/helper.js
@@ -16,3 +16,11 @@ export const getInitials=(name)=>{
 
     return initials.toUpperCase();
 };
+
+export const addThousandsSeparator=(num)=>{
+    if(num==null || isNaN(num)) return "";
+    const [integerPart,fractionPart]=num.toString().split(".");
+    const formattedInteger=integerPart.replace(/\B(?=(\d{3})+(?!\d))/g,",");
+
+    return fractionPart ? `${formattedInteger}.${fractionPart}`:formattedInteger;
+}
