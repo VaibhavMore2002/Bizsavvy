e47c0b70a076725647b121b26b91e1292558eadc
VaibhavMore64 <vaibhav.more@64-squares.com>
2025-05-28 12:40:03 +0530

    Added PieChart

--- frontend/src/pages/Dashboard/Home.jsx
+++ frontend/src/pages/Dashboard/Home.jsx
@@ -9,6 +9,7 @@ import { LuHandCoins,LuWalletMinimal } from "react-icons/lu";
 import {IoMdCard} from "react-icons/io";
 import { addThousandsSeparator } from "../../utils/helper";
 import RecentTransactions from "../../components/Dashboard/RecentTransactions";
+import FinanceOverview from "../../components/Dashboard/FinanceOverview";
 
 const Home=()=>{
   useUserAuth();
@@ -41,7 +42,7 @@ const Home=()=>{
   return(
     <DashboardLayout activeMenu="Dashboard">
       <div className="my-5 mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        {/* <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           <InfoCard
             icon={<IoMdCard/>}
             label="Total Balance"
@@ -60,11 +61,16 @@ const Home=()=>{
             value={addThousandsSeparator(dashboardData?.totalExpenses || 0)}
             color="bg-red-500"
           />
-        </div>
+        </div> */}
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
           <RecentTransactions
             transactions={dashboardData?.recentTransactions}
             onSeeMore={()=>navigate("/expense")}
+          />
+          <FinanceOverview
+            totalBalance={dashboardData?.totalBalance || 0}
+            totalIncome={dashboardData?.totalIncome || 0}
+            totalExpense={dashboardData?.totalExpense || 0}
           />
         </div>
       </div>
